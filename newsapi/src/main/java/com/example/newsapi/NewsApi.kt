41a3f0e605e7bc2bf99b75bb77b11c37f662a482
Bekzat-Chauvbayev <kazna51k@gmail.com>
2024-03-14 13:27:53 +0500

    article,responce, source added and retrofit created

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,8 +1,13 @@
 package com.example.newsapi
 
 import androidx.annotation.IntRange
+import com.example.newsapi.models.Article
 import com.example.newsapi.models.Language
+import com.example.newsapi.models.Response
 import com.example.newsapi.models.SortBy
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
@@ -18,7 +23,25 @@ interface NewsApi {
         @Query("pageSize") @IntRange(from = 0 , to = 100) pageSize : Int =100,
         @Query("page")  @IntRange(from = 1) page : Int =1,
 
-        )
+        ):Response<Article>
+}
+
+fun NewsApi(
+    baseUrl: String,
+    okHttpClient: OkHttpClient? = null
+): NewsApi{
+    val retrofit = retrofit(baseUrl, okHttpClient)
+    return retrofit.create()
+}
+
+private fun retrofit(
+    baseUrl: String,
+    okHttpClient: OkHttpClient?,
+): Retrofit {
+    return  Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .run { if (okHttpClient != null) client(okHttpClient) else this }
+        .build()
 }
 
 
